ec99909ae47f91a82851253c62cb8a7db4bfcb56
Manoitoyai <boomttme@gmail.com>
2024-07-08 15:46:18 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -15,16 +15,16 @@ export default function Home() {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="/About">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Service</a>
+          <a class="nav-link" href="/Service">Service</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="/Contact">Contact</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
